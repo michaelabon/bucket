52998a6cf2212c1fb630005aa528ff8aa2790563
Mike Kenyon <mike.kenyon@gmail.com>
2014-09-06 15:10:51 -0700

    Add item seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+  ['the Creature', 'the Black Lagoon'],
+  ['two turtle doves', 'Puddle'],
+  ['three french hens', 'Puddle'],
+  ['four calling birds', 'Puddle'],
+  ['five golden rings', 'Puddle'],
+  ['six geese a-laying', 'Puddle'],
+  ['seven swans a-swimming', 'Puddle'],
+  ['eight maids a-milking', 'Puddle'],
+  ['nine ladies dancing', 'Puddle'],
+  ['a comeback', 'QwerkyOne'],
+  ['a nice ass-car', 'QwerkyOne'],
+  ['the root password', 'QwerkyOne'],
+  ['a dirty bucket', 'QwerkyOne'],
+  ['a magic wand', 'QwerkyOne'],
+  ['a headcrab', 'Jekotia'],
+  ['42 hikers a-hitching', 'MrMKenyon'],
+  ['root permissions', 'MrMKenyon'],
+].each do |item|
+  Item.find_or_create_by(what: item[0], placed_by: item[1])
+end
