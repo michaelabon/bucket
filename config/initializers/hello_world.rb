1c422bfb165006e793e84e852bfe85cfc9409195
Mike Kenyon <mike.kenyon@gmail.com>
2014-09-07 09:17:22 -0700

    Remove hello world initializer

--- config/initializers/hello_world.rb
+++
@@ -1,5 +0,0 @@
-url = "https://#{Rails.application.secrets[:slack_subdomain]}.slack.com/services/hooks/incoming-webhook?token=#{Rails.application.secrets[:slack_startup_token]}"
-HTTParty.post(
-  url,
-  body: %Q[{"text": "Hello, world!"}]
-)
