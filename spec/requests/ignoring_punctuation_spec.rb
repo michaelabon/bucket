d8c0356aa9c80f75a53b35759ad89709b65944ee
Mike Kenyon <mike.kenyon@gmail.com>
2016-01-19 13:38:40 -0500

    Add additional test case to ignoring punctuation
    [#78772278]

--- spec/requests/ignoring_punctuation_spec.rb
+++ spec/requests/ignoring_punctuation_spec.rb
@@ -13,4 +13,21 @@ describe 'Ignoring punctuation' do
 
     expect(text).to eq reply
   end
+
+  it "fixes issue #78772278 => `you're creepy`" do
+    trigger = "you're creepy"
+    reply = "I'm a product of my environment!"
+
+    slack_post text: "Bucket, #{trigger} <reply> #{reply}"
+
+    expect(text).to eq 'OK, M2K'
+
+    slack_post text: trigger
+
+    expect(text).to eq reply
+
+    slack_post text: 'youre creepy'
+
+    expect(text).to eq reply
+  end
 end
