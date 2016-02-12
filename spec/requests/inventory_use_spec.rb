b3435932e8df4e9e8c259a32af8405b778576e24
Mike Kenyon <mike.kenyon@gmail.com>
2016-02-12 00:05:29 -0500

    Add missing test to default $item case
    When there are no items in the inventory, Bucket should respond with
    'bananas' for each missing item.
    
    [#78772288]

--- spec/requests/inventory_use_spec.rb
+++ spec/requests/inventory_use_spec.rb
@@ -8,15 +8,23 @@ describe "Using an item from Bucket's inventory" do
   context 'when Bucket has an item' do
     before do
       create(:item, what: 'apples')
-      create(:item, what: 'bananas')
+      create(:item, what: 'oranges')
     end
 
     it "replaces all instances of `$item' with an item" do
       slack_post text: 'be helpful'
 
       expect(text)
-        .to eq('I have apples and bananas')
-        .or eq('I have bananas and apples')
+        .to eq('I have apples and oranges')
+        .or eq('I have oranges and apples')
+    end
+  end
+
+  context 'when Bucket does not have an item' do
+    it "replaces all instances of `$item' with `bananas'" do
+      slack_post text: 'be helpful'
+
+      expect(text).to eq('I have bananas and bananas')
     end
   end
 end
