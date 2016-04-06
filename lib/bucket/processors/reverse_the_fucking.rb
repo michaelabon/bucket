4e47793a0525d51196a4e1f93548d55a881652d0
Mike Kenyon <mkenyon@pivotal.io>
2016-04-05 17:23:36 -0700

    Extract the fucking trigger

--- lib/bucket/processors/reverse_the_fucking.rb
+++ lib/bucket/processors/reverse_the_fucking.rb
@@ -2,7 +2,7 @@ module Bucket
   module Processors
     class ReverseTheFucking
       def process(message)
-        return nil unless message.text.scan(regexp).count == 1
+        return nil unless triggered?(message)
 
         if message.text.match regexp
           MessageResponse.new(
@@ -12,6 +12,10 @@ module Bucket
       end
 
       private
+
+      def triggered?(message)
+        message.text.scan(regexp).count == 1
+      end
 
       def regexp
         @regexp ||= /the fucking/
