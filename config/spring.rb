88fbf04139dc9c195dfd79e82dff82b7b8044d00
Michael Abon (Mike Kenyon) <via-github@michaelabon.ca>
2020-12-31 00:00:06 -0600

    Prefer string interpolation in spring.rb

--- config/spring.rb
+++ config/spring.rb
@@ -9,6 +9,6 @@ Spring.after_fork do
   ENV['DEBUGGER_STORED_RUBYLIB']&.split(File::PATH_SEPARATOR)&.each do |path|
     next unless path =~ /ruby-debug-ide/
 
-    load path + '/ruby-debug-ide/multiprocess/starter.rb'
+    load "#{path}/ruby-debug-ide/multiprocess/starter.rb"
   end
 end
