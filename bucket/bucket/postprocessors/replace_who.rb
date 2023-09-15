8aa71bbd23f8ee1a0bbb5fc9ff9e90c8d2368675
Michael Abon <via-github@michaelabon.ca>
2023-09-15 11:15:08 -0500

    Update bucket to allow for new cops

--- bucket/bucket/postprocessors/replace_who.rb
+++ bucket/bucket/postprocessors/replace_who.rb
@@ -4,7 +4,7 @@ module Bucket
       def process(message_response)
         return unless message_response&.text && message_response&.user_name
 
-        message_response.text.gsub!(/\$who/, message_response.user_name)
+        message_response.text.gsub!('$who', message_response.user_name)
       end
     end
   end
