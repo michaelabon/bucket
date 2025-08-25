2103072bfe9c1907110088508b1d9f7fdb014aea
Michael Abon <via-github@michaelabon.ca>
2025-08-25 09:34:51 -0500

    Opt in to Rails 8.1 time zone handling

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -6,10 +6,6 @@
 #
 # Read the Rails 5.0 release notes for more info on each option.
 
-# Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
-# Previous versions had false.
-ActiveSupport.to_time_preserves_timezone = true
-
 # Require `belongs_to` associations by default. Previous versions had false.
 Rails.application.config.active_record.belongs_to_required_by_default = true
 
